68cbf99ad40d906d62380456fa3a5d5db8fe957c
Martín Natera <annielnatera2.0@gmail.com>
2025-08-21 13:50:35 -0400

    segunda prueba

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 /*------------------------------LIBRERIAS UTILIZADAS--------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h> 
+#include <string.h>
 #include <ctype.h>
 
 /*-------------------------------LIMITES Y CONSTANTES-------------------------------*/
@@ -20,6 +20,11 @@
 
 //estructura de tiempo: solo incorpora segundos,minutos y horas.
 typedef struct tm tiempo;
+
+//estructura de cantidades.
+typedef struct{
+	int cancion,show,publicidad;
+}cantidad;
 
 //posible estructura Auxiliar para el terminal y busqueda y grilla
 typedef struct{
@@ -52,7 +57,18 @@ typedef struct{
 
 /*--------------------------------DECLARACION DE FUNCIONES---------------------------------*/
 
-void mostrarArraysGrilla(grilla g[],int cant_grilla);
+// aleatoriedad en publicidad.
+void elepcion_publicidades(grilla* grilla_dia,int cantPubli,Publicidad publicidades[],tiempo* t);
+// aleatoriedad en canciones.
+void elepcion_canciones(grilla* grilla_dia,int cantCanc,cancion canciones[],tiempo* t);
+// aleatoriedad en shows.
+void elepcion_shows(grilla* grilla_dia,int cantShows,show shows[],tiempo* t);
+// reinicia tiempo a tiempo inicial 00:05:00
+tiempo tiempo_inicial(tiempo tiempoInicial);
+
+void creacion_grilla(int c_h,int c_c,int c_p,show h[],cancion c[],Publicidad p[],grilla grilla_dia[],int *cant_elem);
+
+
 void llenar_grilla(show shows[],Publicidad publicidades[],cancion canciones[]
 	,tiempo* timeInicial,grilla result[]);
 
@@ -66,11 +82,16 @@ int aleatorio_en_rango(int min, int max);
 // acomda horas,minutos y segundos.
 void convertir_a_hhmmss(int minutos, int segundos,tiempo* tiempoI);
 
+void aleatoriedad_de_grilla(
+			int numAleatorio,int posicionG,int posicionO,
+			show shows[],Publicidad publicidades[],cancion canciones[]
+			,tiempo* timeInicial,grilla result[]
+		);
+
 /*-------------------CREACION DE LAS GRILLAS-------------------------*/
 
 // Posible funcion para las grillas de cada dia.
 void grilla_dia(grilla horario[],int dia);
-
 // Posible funcion para las grillas de terminal.
 void grilla_principal(show shows[],Publicidad publicidades[],cancion canciones[]
 	,tiempo* timeInicial,grilla result[]);
@@ -78,19 +99,20 @@ void grilla_principal(show shows[],Publicidad publicidades[],cancion canciones[]
 
 /*-------------------FUNCIONES DE ANALISIS DE ERROR-------------------------*/
 
-// muestra el contenido de una Array de canciones.
+// muestra el contenido de una Array de canciones en terminal.
 void mostrarArraysCanciones(cancion can[],int cant_canciones);
-
+// muestra el contenido de una Array de shows en terminal.
 void mostrarArraysShows(show sh[],int cant_canciones);
-
+// muestra el contenido de una Array de publicidades en terminal.
 void mostrarArraysPublicidades(Publicidad pub[],int cant_pub);
+// muestra el contenido de una Array de grilla en terminal.(Una fraccion)
+void mostrarArraysGrilla(grilla g[],int cant_grilla);
 
 
 /*------------------FUNCIONES DE ORDENAMIENTO DE DATOS-------------------------*/
 
 // ordena de menor a mayor puntaje cada cancion.
 void ordenar_lista_de_canciones(cancion canciones[],int tamaño);
-
 // ordena de menor a mayor por raiting de cada show.
 void ordenar_lista_de_show(show showRadiales[]);
 
@@ -98,19 +120,17 @@ void ordenar_lista_de_show(show showRadiales[]);
 /*-------------------FUNCIONES DE REGISTRO DE DATOS-------------------------*/
 
 //funcion engargada de llenar arreglos de shows
-void lecturaDeShow(show showRadiales[]);
-
+void lecturaDeShow(show showRadiales[],int* cant_shows);
 //funcion engargada de llenar arreglos de canciones.
-void lecturaDeCanciones(cancion canciones[]);
-
+void lecturaDeCanciones(cancion canciones[],int* cant_canciones );
 //funcion engargada de llenar arreglos de publicidades. 
-void lecturaDePublicidad(Publicidad publicidades[]);
+void lecturaDePublicidad(Publicidad publicidades[],int* cant_publicida);
 
 
 
 
 
-/*----------------------------------CUERPO PRINCIPAL-------------------------------------*/
+/*----------------------------------MAIN-------------------------------------*/
 
 int main(){
 	
@@ -127,28 +147,31 @@ int main(){
 	cancion canciones[MAX_CANCIONES];
 	show shows[MAX_SHOW];
 	Publicidad publicidades[MAX_PUBLICIDADES];
-	grilla result[1000];
 
+
+	grilla result[1000];
+	grilla matriz[50][7];
+
+	grilla grilla_aux[100][3];
 
 	/*---------------------AREA DE PRUEBA--------------------------*/
 
-	lecturaDeCanciones(canciones);
-	lecturaDeShow(shows);
-	lecturaDePublicidad(publicidades);
+	int cant_shows,cant_canciones,cant_publicidades;
+	
+	lecturaDeCanciones(canciones,&cant_canciones);
+	lecturaDeShow(shows,&cant_shows);
+	lecturaDePublicidad(publicidades,&cant_publicidades);
 
-	llenar_grilla(shows,publicidades,canciones,&tiempoInicial,result);
+	//llenar_grilla(shows,publicidades,canciones,&tiempoInicial,result);
+	int cant_dia;
+	creacion_grilla(cant_shows,cant_canciones,cant_publicidades,shows,canciones,publicidades,result,&cant_dia);
+	mostrarArraysGrilla(result,cant_dia);
 
-	printf("%-3c %-3c %-3c %-2c %s\n",'H','M','S','T',"nombre:");
-	// mostrarArraysCanciones(canciones,9);
-	//mostrarArraysPublicidades(publicidades,2);
-	mostrarArraysGrilla(result,4);
-
-	/*	printf("despues:\n");
-	ordenar_lista_de_canciones(canciones,2);
-	mostrarArraysCanciones(canciones,sizeof(canciones)/sizeof(canciones[0]));*/
-
+	//mostrarArraysCanciones(canciones,cant_canciones);
+	//mostrarArraysShows(shows,cant_shows);
 	
 
+	
 	return 0;
 }
 
@@ -156,10 +179,101 @@ int main(){
 
 
 
-/*---------------------------------CODIGO DE FUNCIONES---------------------------------*/
+/*---------------------------------IMPLEMENTACION---------------------------------*/
 
+void elepcion_shows(grilla* grilla_dia,int cantShows,show shows[],tiempo* t){
+	int num = aleatorio_en_rango(1,cantShows);
 
+	grilla_dia->tipo = 'S';
+	strcpy(grilla_dia->nombre,shows[num].name);
+	convertir_a_hhmmss(shows[num].min,shows[num].seg,&*t);
+	grilla_dia->tiempoS = *t ;
+}
 
+void elepcion_canciones(grilla* grilla_dia,int cantCanc,cancion canciones[],tiempo* t){
+	int num = aleatorio_en_rango(1,cantCanc);
+
+	grilla_dia->tipo = 'C';
+	strcpy(grilla_dia->nombre,canciones[num].name);
+	convertir_a_hhmmss(canciones[num].min,canciones[num].seg,&*t); 
+	grilla_dia->tiempoS = *t;
+}
+
+void elepcion_publicidades(grilla* grilla_dia,int cantPubli,Publicidad publicidades[],tiempo* t){
+	int num = aleatorio_en_rango(1,cantPubli);
+
+	grilla_dia->tipo = 'P';
+	strcpy(grilla_dia->nombre,publicidades[num].name);
+	convertir_a_hhmmss(0,publicidades[num].seg,&*t); 
+	grilla_dia->tiempoS = *t;
+}
+
+// contador de tiempo para grilla.
+tiempo tiempo_inicial(tiempo tiempoInicial){
+
+	// tiempo inicial: 00:05:00
+	tiempoInicial.tm_hour = 0;
+	tiempoInicial.tm_min = 5;
+	tiempoInicial.tm_sec = 0;
+
+	return tiempoInicial;
+}
+
+// Creacion de la grilla y cant de elementos de grilla.
+void creacion_grilla(int c_h,int c_c,int c_p,show h[],cancion c[],Publicidad p[],grilla grilla_dia[],int *cant_elem){
+
+	// contador de tiempo para grilla.
+	tiempo tiempoInicial = tiempo_inicial(tiempoInicial);
+	int numAleatorio,pos_grilla=0;
+
+	// probar con tres 24:00:00
+	while( tiempoInicial.tm_hour < 12 ){
+		numAleatorio = aleatorio_en_rango(1,3);//posible elepcion entre shows,canciones y publiciades.
+		switch(numAleatorio){
+			case 1:
+				elepcion_shows(&grilla_dia[pos_grilla],c_h,h,&tiempoInicial);
+				break;
+			case 2:
+				elepcion_canciones(&grilla_dia[pos_grilla],c_c,c,&tiempoInicial);
+				break;
+			default:
+				elepcion_publicidades(&grilla_dia[pos_grilla],c_p,p,&tiempoInicial);
+				break;
+		}
+		pos_grilla++;
+	}
+	*cant_elem = pos_grilla;
+}
+
+void aleatoriedad_de_grilla(
+			int numAleatorio,int posicionG,int posicionO,
+			show shows[],Publicidad publicidades[],cancion canciones[]
+			,tiempo* timeInicial,grilla result[]
+		)
+{
+		switch(numAleatorio){
+			case 0:
+				result[posicionG].tipo = 'S';
+				strcpy(result[posicionG].nombre, shows[posicionO].name);
+				convertir_a_hhmmss(shows[posicionO].min,shows[posicionO].seg,&*timeInicial); 
+				result[posicionG].tiempoS = *timeInicial;
+				break;
+			case 1:
+				result[posicionG].tipo = 'C';
+				strcpy(result[posicionG].nombre, canciones[posicionO].name);
+				convertir_a_hhmmss(canciones[posicionO].min,canciones[posicionO].seg,&*timeInicial); 
+				result[posicionG].tiempoS = *timeInicial;
+				break;
+			default:
+				result[posicionG].tipo = 'P';
+				strcpy(result[posicionG].nombre, publicidades[posicionO].name);
+				convertir_a_hhmmss(0,publicidades[posicionO].seg,&*timeInicial); 
+				result[posicionG].tiempoS = *timeInicial;
+				break;
+		}
+}
+
+// llena una grilla(protipo).
 void llenar_grilla(show shows[],Publicidad publicidades[],cancion canciones[]
 	,tiempo* timeInicial,grilla result[])
 {
@@ -168,31 +282,16 @@ void llenar_grilla(show shows[],Publicidad publicidades[],cancion canciones[]
 
 	while( i < 4 ){
 
-		switch( aleatorio_en_rango(0,2) ){
-			case 0:
-				result[i].tipo = 'S';
-				strcpy(result[i].nombre, shows[i].name);
-				convertir_a_hhmmss(shows[i].min,shows[i].seg,&*timeInicial); 
-				result[i].tiempoS = *timeInicial;
-				break;
-			case 1:
-				result[i].tipo = 'C';
-				strcpy(result[i].nombre, canciones[i].name);
-				convertir_a_hhmmss(shows[i].min,canciones[i].seg,&*timeInicial); 
-				result[i].tiempoS = *timeInicial;
-				break;
-			default:
-				result[i].tipo = 'P';
-				strcpy(result[i].nombre, publicidades[i].name);
-				convertir_a_hhmmss(publicidades[i].min,0,&*timeInicial); 
-				result[i].tiempoS = *timeInicial;
-				break;
-		}
+		aleatoriedad_de_grilla(aleatorio_en_rango(0,2),
+			i,i,shows,publicidades,
+			canciones,&*timeInicial,result);
 
 		i++;
 	}
 }
 
+
+// Crear la grilla de cada dia(prototipo).
 void grilla_principal(show shows[],Publicidad publicidades[],cancion canciones[]
 	,tiempo* timeInicial,grilla result[])
 {
@@ -217,7 +316,7 @@ void grilla_principal(show shows[],Publicidad publicidades[],cancion canciones[]
 }
 
 
-/*-------------------FUNCIONES COMPLEMENTARIAS-------------------------*/
+/*-------------------------FUNCIONES COMPLEMENTARIAS---------------------------*/
 
 //funcion para identificar las horas de shows especiales.
 int showsEspeciales(int num){
@@ -228,8 +327,7 @@ int showsEspeciales(int num){
 	return res;
 }
 
-
-// Función que genera un número aleatorio entre min y max (inclusive)
+// Función que genera un numero aleatorio entre min y max (inclusive)
 int aleatorio_en_rango(int min, int max){
     // Asegurarse de que min <= max
     if (min > max) {
@@ -242,15 +340,23 @@ int aleatorio_en_rango(int min, int max){
     return min + rand() % (max - min + 1);
 }
 
-
+// Ajusta el tiempo de forma estandar de h:mn:seg
 void convertir_a_hhmmss(int minutos, int segundos,tiempo* tiempoI){
     
     // Asigna los segundos y minutos
     tiempoI->tm_sec += segundos;
     tiempoI->tm_min += minutos;
+
+	// Asegurar que la estructura está completa
+    if (tiempoI->tm_year == 0) {
+        tiempoI->tm_year = 2024 - 1900;
+        tiempoI->tm_mon = 0;
+        tiempoI->tm_mday = 1;
+    }
+    
     
     // Normaliza la estructura (ajusta horas, minutos, segundos)
-    mktime(&*tiempoI); // Esta función ajusta los valores
+    mktime(&*tiempoI); // Esta funcion ajusta los valores
 }
 
 /*------------------FUNCIONES DE ORDENAMIENTO DE DATOS-------------------------*/
@@ -291,14 +397,21 @@ void ordenar_lista_de_shows(show shows[],int tamaño){
 
 }
 
-//muestra el array de canciones.
+/*------------------FUNCIONES DE OPSERVACION EN TERMINAL-------------------------*/
+
+//muestra en el terminal el array de canciones.
 void mostrarArraysCanciones(cancion can[],int cant_canciones){
 	for( int i=0; i < cant_canciones;i++){
-		printf("%s %d %d %d\n",can[i].name,can[i].min,can[i].seg,can[i].puntuacion);
+		printf("%s %d %d %d\n",
+					can[i].name,
+					can[i].min,
+					can[i].seg,
+					can[i].puntuacion
+				);
 	}
 }
 
-//muestra el array de shows.
+//muestra en el terminal el array de shows.
 void mostrarArraysShows(show sh[],int cant_canciones){
 	for( int i=0; i < cant_canciones;i++){
 		printf("%s %d %d %d %d\n",
@@ -311,7 +424,7 @@ void mostrarArraysShows(show sh[],int cant_canciones){
 	}
 }
 
-//muestra el array de publicidades.
+//muestra en el terminal el array de publicidades.
 void mostrarArraysPublicidades(Publicidad pub[],int cant_pub){
 	for( int i=0; i < cant_pub;i++){
 		printf("%s %d %d \n",
@@ -322,10 +435,11 @@ void mostrarArraysPublicidades(Publicidad pub[],int cant_pub){
 	}
 }
 
-//muestra el array de grilla.
+//muestra en el terminal el array de grilla.
 void mostrarArraysGrilla(grilla g[],int cant_grilla){
+	printf("%-3c %-3c %-3c %-2c %s\n",'H','M','S','T',"nombre:");
 	for( int i=0; i < cant_grilla;i++){
-
+		//muestra en pantalla los resultados.
 		printf("%-3d %-3d %-3d %-2c %s\n",
 				g[i].tiempoS.tm_hour,
 				g[i].tiempoS.tm_min,
@@ -338,7 +452,7 @@ void mostrarArraysGrilla(grilla g[],int cant_grilla){
 
 /*-------------------FUNCIONES DE REGISTRO DE DATOS-------------------------*/
 
-void lecturaDeCanciones(cancion canciones[]) {
+void lecturaDeCanciones(cancion canciones[],int* cant_canciones ){
     FILE *lista_canciones = fopen("canciones.in", "r");
     
     if (lista_canciones == NULL) {
@@ -346,15 +460,15 @@ void lecturaDeCanciones(cancion canciones[]) {
         return;
     }
 
-    int cantidad;
-    fscanf(lista_canciones,"%d", &cantidad);
+
+    fscanf(lista_canciones,"%d", &*cant_canciones);
     
-    // Leer el salto de línea después del número
+    // Leer el salto de línea después del numero
     char temp;
     fscanf(lista_canciones, "%c", &temp); 
 
-    for (int i = 0; i < cantidad; i++) {
-        // Leer el nombre hasta encontrar un número
+    for (int i = 0; i < *cant_canciones; i++) {
+        // Leer el nombre hasta encontrar un numero
         int pos = 0;
         char nombre[100];
         char letra;
@@ -384,7 +498,7 @@ void lecturaDeCanciones(cancion canciones[]) {
     fclose(lista_canciones);
 }
 
-void lecturaDeShow(show showRadiales[]){
+void lecturaDeShow(show showRadiales[],int* cant_shows){
     FILE *lista_shows = fopen("shows.in", "r");
     
     if (lista_shows == NULL) {
@@ -392,15 +506,15 @@ void lecturaDeShow(show showRadiales[]){
         return;
     }
 
-    int cantidad;
-    fscanf(lista_shows,"%d", &cantidad);
+
+    fscanf(lista_shows,"%d", &*cant_shows);
     
-    // Leer el salto de línea después del número
+    // Leer el salto de línea después del numero
     char temp;
     fscanf(lista_shows, "%c", &temp); 
 
-    for (int i = 0; i < cantidad; i++) {
-        // Leer el nombre hasta encontrar un número
+    for (int i = 0; i < *cant_shows; i++) {
+        // Leer el nombre hasta encontrar un numero
         int pos = 0;
         char nombre[100];
         char letra;
@@ -432,7 +546,7 @@ void lecturaDeShow(show showRadiales[]){
     fclose(lista_shows);
 }
 
-void lecturaDePublicidad(Publicidad publicidades[]){
+void lecturaDePublicidad(Publicidad publicidades[],int* cant_publicida){
 	//Archivo de entrada de show.
 	FILE *lista_publicidades = fopen("publicidad.in","r");
 
@@ -442,12 +556,13 @@ void lecturaDePublicidad(Publicidad publicidades[]){
 	if(lista_publicidades != NULL){
 		//loop.
 		while(!feof(lista_publicidades)){
-			// Leer el nombre hasta encontrar un número
+			// Leer el nombre hasta encontrar un numero
 			int pos = 0;
 			char nombre[100];
 			char letra;
 			
 			fscanf(lista_publicidades, "%c", &letra);
+			// loop interno:
 			while(!isdigit(letra) && pos < 99 && !feof(lista_publicidades)) {
 				nombre[pos] = letra;
 				pos++;
@@ -465,6 +580,7 @@ void lecturaDePublicidad(Publicidad publicidades[]){
 				i++;
 			}
 		}
+		*cant_publicida = i;
 	}
 
 	//cerrado de archivos.
